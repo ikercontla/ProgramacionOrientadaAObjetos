d1b8bf1dd2414844775466b68f5d351a44a576ae
Iker Contla <iker.contla@iteso.mx>
2024-08-21 22:31:50 -0600

    hjola

--- EjerciciosEnClaseSesion04/src/EjerciciosSesion04.java
+++ EjerciciosEnClaseSesion04/src/EjerciciosSesion04.java
@@ -3,76 +3,76 @@ import javax.swing.JOptionPane;
 public class EjerciciosSesion04 {
 
 	public static void main(String[] args) {
-//		
-//		// Ejercicio 1
-//		// Solicite de forma separada nombre, apellido paterno y materno
-//		
-//		String nombre = JOptionPane.showInputDialog("Escribe tu nombre: ");
-//		String apellidoPaterno = JOptionPane.showInputDialog("Escribe tu apellido paterno: ");
-//		String apellidoMaterno = JOptionPane.showInputDialog("Escribe tu apellido materno: ");
-//		
-//		System.out.printf("Tu nombre es: %s %s, %s", apellidoPaterno.toUpperCase(), apellidoMaterno.toUpperCase(), nombre.toUpperCase());
-//		
-//		// Ejercicio 2
-//		// Solicite al usuario los datos de su viaje
-//		
-//		String velocidadPromedio = JOptionPane.showInputDialog("Escribe la velocidad promedio de su viaje en km por hr: ");
-//		String distanciaQueFalta = JOptionPane.showInputDialog("Escribe la distancia que falta en kilometros: ");
-//		String cuantoTiempoParaLlegar = JOptionPane.showInputDialog("Escribe en cuanto tiempo tienes que llegar en minutos: ");
-//		
-//		// Convertimos esos datos a valores numericos
-//		
-//		int velocidadPromedioValorNumerico = Integer.parseInt(velocidadPromedio);
-//		int distanciaQueFaltaValorNumerico = Integer.parseInt(distanciaQueFalta);
-//		int cuantoTiempoParaLlegarValorNumerico = Integer.parseInt(cuantoTiempoParaLlegar);
-//		
-//		// Pasamos el tiempo que le falta para llegar a horas 
-//		long cuantoTiempoParaLlegarEnHoras = cuantoTiempoParaLlegarValorNumerico/60;
-//		
-//		//Calculamos en cuanto tiempo llega
-//		long llegarATiempo = distanciaQueFaltaValorNumerico / velocidadPromedioValorNumerico;
-//		
-//		// Hacemos un if para comparar el tiempo que le falta con el tiempo en que llegara
-//		if (llegarATiempo <= cuantoTiempoParaLlegarEnHoras ) {
-//			JOptionPane.showMessageDialog(null,
-//											"Si Llegas",
-//											"LLegaste",
-//										   JOptionPane.INFORMATION_MESSAGE);
-//		}
-//		else{
-//			JOptionPane.showMessageDialog(null, 
-//											"Ya no llegaste",
-//										   "Llegaste?",
-//										   JOptionPane.WARNING_MESSAGE);
-//		}
-//		
-//		// Ejercicio 3
-//		//Solicite al usuario su lenguaje de programacion favorito 
-//		//Si es java imprimir "su lenguaje es el mas popular"
-//		//Si es C imprimir "su lenguaje es el segundo mas popular
-//		//Si es C++ imprimir "su lenguaje es el tercero mas popular"
-//		//En caso de que sea otro imprimir "Su lenguaje no es popular o no existe"
-//		
-//		//solicitamos al usuario su lenguaje de programacion favorito
-//		String lenguajeFavorito = JOptionPane.showInputDialog("Cual es su lenguaje de programacion favorito?");
-//		
-//		// Convertimos la respuesta a todo minusculas para que podamos compararlo a una cadena mas facilmente
-//		String lenguajeFavoritoMinusculas = lenguajeFavorito.toLowerCase();
-//		
-//		//Comparamos a java, C y c++ con el metodo equals
-//		
-//		if (lenguajeFavoritoMinusculas.equals("java")) {
-//			System.out.printf("su lenguaje de programacion es el mas popular");
-//		}
-//		else if (lenguajeFavoritoMinusculas.equals("c")) {
-//			System.out.printf("Su lenguaje de programacion es el segundo mas popular");
-//		}
-//		else if (lenguajeFavoritoMinusculas.equals("c++")) {
-//			System.out.printf("Su lenguaje de programacion es el tercero mas popular");
-//		}
-//		else {
-//			System.out.printf("Su lenguaje no es popular o no existe");
-//		}
+		
+		// Ejercicio 1
+		// Solicite de forma separada nombre, apellido paterno y materno
+		
+		String nombre = JOptionPane.showInputDialog("Escribe tu nombre: ");
+		String apellidoPaterno = JOptionPane.showInputDialog("Escribe tu apellido paterno: ");
+		String apellidoMaterno = JOptionPane.showInputDialog("Escribe tu apellido materno: ");
+		
+		System.out.printf("Tu nombre es: %s %s, %s", apellidoPaterno.toUpperCase(), apellidoMaterno.toUpperCase(), nombre.toUpperCase());
+		
+		// Ejercicio 2
+		// Solicite al usuario los datos de su viaje
+		
+		String velocidadPromedio = JOptionPane.showInputDialog("Escribe la velocidad promedio de su viaje en km por hr: ");
+		String distanciaQueFalta = JOptionPane.showInputDialog("Escribe la distancia que falta en kilometros: ");
+		String cuantoTiempoParaLlegar = JOptionPane.showInputDialog("Escribe en cuanto tiempo tienes que llegar en minutos: ");
+		
+		// Convertimos esos datos a valores numericos
+		
+		int velocidadPromedioValorNumerico = Integer.parseInt(velocidadPromedio);
+		int distanciaQueFaltaValorNumerico = Integer.parseInt(distanciaQueFalta);
+		int cuantoTiempoParaLlegarValorNumerico = Integer.parseInt(cuantoTiempoParaLlegar);
+		
+		// Pasamos el tiempo que le falta para llegar a horas 
+		long cuantoTiempoParaLlegarEnHoras = cuantoTiempoParaLlegarValorNumerico/60;
+		
+		//Calculamos en cuanto tiempo llega
+		long llegarATiempo = distanciaQueFaltaValorNumerico / velocidadPromedioValorNumerico;
+		
+		// Hacemos un if para comparar el tiempo que le falta con el tiempo en que llegara
+		if (llegarATiempo <= cuantoTiempoParaLlegarEnHoras ) {
+			JOptionPane.showMessageDialog(null,
+											"Si Llegas",
+											"LLegaste",
+										   JOptionPane.INFORMATION_MESSAGE);
+		}
+		else{
+			JOptionPane.showMessageDialog(null, 
+											"Ya no llegaste",
+										   "Llegaste?",
+										   JOptionPane.WARNING_MESSAGE);
+		}
+		
+		// Ejercicio 3
+		//Solicite al usuario su lenguaje de programacion favorito 
+		//Si es java imprimir "su lenguaje es el mas popular"
+		//Si es C imprimir "su lenguaje es el segundo mas popular
+		//Si es C++ imprimir "su lenguaje es el tercero mas popular"
+		//En caso de que sea otro imprimir "Su lenguaje no es popular o no existe"
+		
+		//solicitamos al usuario su lenguaje de programacion favorito
+		String lenguajeFavorito = JOptionPane.showInputDialog("Cual es su lenguaje de programacion favorito?");
+		
+		// Convertimos la respuesta a todo minusculas para que podamos compararlo a una cadena mas facilmente
+		String lenguajeFavoritoMinusculas = lenguajeFavorito.toLowerCase();
+		
+		//Comparamos a java, C y c++ con el metodo equals
+		
+		if (lenguajeFavoritoMinusculas.equals("java")) {
+			System.out.printf("su lenguaje de programacion es el mas popular");
+		}
+		else if (lenguajeFavoritoMinusculas.equals("c")) {
+			System.out.printf("Su lenguaje de programacion es el segundo mas popular");
+		}
+		else if (lenguajeFavoritoMinusculas.equals("c++")) {
+			System.out.printf("Su lenguaje de programacion es el tercero mas popular");
+		}
+		else {
+			System.out.printf("Su lenguaje no es popular o no existe");
+		}
 
 		//Ejercicio 4 
 		// Solicitar al usuario: Nombre del mes y year
@@ -104,9 +104,9 @@ public class EjerciciosSesion04 {
 		case "noviembre" : diasMes = "30"; break;
 		case "diciembre" : diasMes = "31"; break;
 		case "febrero" : 
-			if ((yearValorNumerico % 4 == 0 && yearValorNumerico % 100 != 0) || (yearValorNumerico % 400 == 0)) {
+			if ((yearValorNumerico % 4 == 0 && yearValorNumerico % 100 != 0) || (yearValorNumerico % 400 == 0)) { //determinamos si el year es bisiesto
 				System.out.printf("El year es bisiesto \n");
-				diasMes = "29";
+				diasMes = "29"; // si si lo es el mes tendria 29 dias
 			}
 			else { 
 				System.out.printf("El year no es bisiesto \n");
